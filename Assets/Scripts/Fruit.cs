8cd6e03a119815f8b1d6c498028253035e71f8a4
uwkd <86751557+uwkd@users.noreply.github.com>
2026-04-15 14:55:52 +0700

    Update Fruit.cs

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -2,17 +2,16 @@
 
 public class Fruit : MonoBehaviour
 {
-    [SerializeField] private int scoreValue = 1; // Số điểm cộng thêm khi ăn
+    [SerializeField] private int scoreValue = 1; 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Kiểm tra xem thứ vừa chạm vào mình có phải là Player không
+
         if (collision.CompareTag("Player"))
         {
-            // Sau này Trang sẽ gọi hàm tăng điểm từ một Script quản lý điểm ở đây
+
             Debug.Log("Đã ăn trái cây! Cộng: " + scoreValue + " điểm.");
 
-            // Xóa quả dâu đi
             Destroy(gameObject);
         }
     }
